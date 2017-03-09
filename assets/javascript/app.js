d8ae24038b55f2dd35726971df55d08118171a6f
MLangteau <langteauM@gmail.com>
2017-03-08 23:33:37 -0500

    displays 10 stills

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,99 +1,114 @@
-//    api.giphy.com
-//     dc6zaTOxFJmzC
-//    q, limit, rating
-
-// Initial array of fish
-var fishes = ["Gar", "Jaws", "Marlin", "Goldfish", "Clownfish", "Siamese fighting fish", "Haddock", "Gar", "Eel", "Snook", "Guppy", "Megalodon"];
-
-// This function re-renders the HTML to display the appropriate content
-function displayFishInfo() {
-
-  var fish = $(this).attr("data-name");
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + fish + "&api_key=dc6zaTOxFJmzC";
-
- // var queryURL = "http://www.omdbapi.com/?t=" + fish + "&y=&plot=short&r=json";
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response) {
-    //  Create div to hold the fish
-    console.log("Response ", response);
-    var fishDiv = $("<div class='fish-item'>");
-
-          // Storing the rating data     ???????????????????????????????  1
-        var rated = response.data[0].rating;
-
-          // Creating an element to have the rating displayed   ???????????????  2
-        var pOne = $("<p>").text("Rating: " + rated);
-
-        // displayint the fish rating              ????????????????????????  3
-        fishDiv.append(pOne);
-
-    // store the fish picture/giffy
-//    var giffy = response.??????;
-        // Retrieving the URL for the image
-
-    //    var imgURL = response.data[0].bitly_gif_url;
-        var imgURL = response.data[0].images.downsized_still.url;
-
-
-        // Creating an element to hold the image
-        var image = $("<img>").attr("src", imgURL);
-
-        // Appending the image
-        fishDiv.append(image);
-
-        // put last fish before the other fish (first)
-     //////   $("#fishes-view").prepend(fishDiv);
-
-      $("#fishes-view").prepend(fishDiv);
-
-  });
-};
-
-function renderButtons() {
-
-//  keeps from having multiples of same buttons
-  $("#buttons-view").empty();
-
-// generates buttons dynamically for each fish in original array (don't need end tag 4 button)
-  for (var i = 0; i < fishes.length; i++) {
-    // make sure you do NOT, I REPEAT DO NOT USE #<button>
-    var a = $("<button>");
-    // add class of "fish" to out button
-    a.addClass("fish");
-    // add data attribute 
-    a.attr("data-name", fishes[i]);
-    // Providing the initial button text
-    a.text(fishes[i]);
-    // Adding a button to the buttons-view div
-    $("#buttons-view").append(a);
-  };  // end of for
-};  // end of renderButtons function
-
-  // This function handles events where a fish button is clicked
-  $("#add-fish").on("click", function(event) {
-    event.preventDefault();
-    // This line grabs the input from the textbox
-    var fish = $("#fish-input").val().trim();
-
-    // Adding fish from the textbox to our array
-    fishes.push(fish);
-    console.log(fish);
-
-    // Calling renderButtons which handles the processing of our fish array
-    renderButtons();
-  });
-
-
 //  This code will run as soon as the page loads.
 $(document).ready(function() {
 
+    // Initial array of fish
+    var topics = ["Marlin", "Sailfish", "Clownfish", "Siamese fighting fish", "Haddock", "Aligator Gar", "Eel", "Snook", "Guppy", "Megalodon"];
+
+    // This function re-renders the HTML to display the appropriate content
+    function displayFishInfo() {
+
+      var fish = $(this).attr("data-name");
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + fish + "&limit=10&api_key=dc6zaTOxFJmzC";
+
+    //   var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + fish + "&api_key=dc6zaTOxFJmzC";
+
+       $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).done(function(response) {
+            //  Create div to hold the fish
+            console.log("Response ", response);
+
+        //  clears the previous 10 images
+        $("#topics-view").empty();
+
+        for (i=0; i<response.data.length; i++) {
+            // store the fish picture/giffy
+            var fishDiv = $("<div class='fish-item'>");
+
+    //*********************************************************************
+
+            // Retrieving the URL for the image
+            var imgURL = response.data[i].images.original_still.url;
+
+            // Creating an element to hold the image
+            var image = $("<img>").attr("src", imgURL);
+
+            // Appending the image to the above element
+            image.attr(alt='"image" + i');
+            image.attr("data-still", response.data[i].images.original_still.url);
+            image.attr("data-animate", response.data[i].images.original.url);
+
+            // Appending the image to the above element
+            fishDiv.append(image);
+
+            // Storing the rating in the rated variable   
+            var rated = response.data[i].rating;
+
+            // Creating an element to have the rating displayed   
+            var para = $("<p>").text("Rating: " + rated);
+
+            para.addClass('ratings');
+
+            // appends the rating to the element with other items for each image  
+            fishDiv.append(para);
+          //  puts the last topic clicked before the other already displayed images
+          $("#topics-view").prepend(fishDiv);
+        }
+
+      });
+    };
+
+    function renderButtons() {
+
+    //  keeps from having multiples of same buttons
+      $("#buttons-view").empty();
+
+    // generates buttons dynamically for each fish in original array (don't need end tag 4 button)
+      for (var i = 0; i < topics.length; i++) {
+        // make sure you do NOT, I REPEAT DO NOT USE #<button>
+        var a = $("<button>");
+        // add class of "fish" to out button
+        a.addClass("fish");
+        // add data attribute 
+        a.attr("data-name", topics[i]);
+        // Providing the initial button text
+        a.text(topics[i]);
+        // Adding a button to the buttons-view div
+        $("#buttons-view").append(a);
+      };  // end of for
+    };  // end of renderButtons function
+
+      // This function handles events where a fish button is clicked
+      $("#add-fish").on("click", function(event) {
+
+        event.preventDefault();
+
+        // This line grabs the input from the textbox
+        var fish = $("#fish-input").val().trim();
+
+        if (fish) {
+
+        // Adding fish from the textbox to our array
+        topics.push(fish);
+
+        // Calling renderButtons which handles the processing of our fish array
+        renderButtons();
+        }
+        else
+        {
+          return false;
+        }
+
+
+        // clears out the input after pressing enter/submit
+        $("#fish-input").val("");
+      });
+
 /// Adding an event listener to all elements with "fish" class dynamically created
-$(document).on("click", ".fish", displayFishInfo);
+    $(document).on("click", ".fish", displayFishInfo);
 
 // Calls the renderButtons function to display initial buttons
-renderButtons();
+    renderButtons();
 
 }); // end of document.ready
